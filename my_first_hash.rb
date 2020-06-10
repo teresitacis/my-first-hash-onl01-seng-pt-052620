2bf22a8fc44524cc9c4359f551386b111b7ea836
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 02:50:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-+ your_hash = {"kitty" => "Nikitty"}
-
--  
+my_hash = {"Name"=> "Marine Boudeau","Age" =>"32", "Status" => "Single"}
 end
 
 
